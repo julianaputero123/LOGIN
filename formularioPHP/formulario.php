871d187506424fbad9d55468c6f77c4ac1d6c149
juliana <juliana@dc.exa.unrc.edu.ar>
2024-05-24 14:14:19 -0300

    formulario

---
+++ formularioPHP/formulario.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+        <form action='recogerdatosformulario.php' method=POST>
+            Nombre: <input type='text' name=nombre>
+            Email: <input type="text" name='email'>
+            <input type='submit' value='enviar'>
+        <form>    
+    </body>
+
+<html>
